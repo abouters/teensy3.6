e1ef76b1f7dc7662e5eac5f902ab146958e12e9f
dwanson <k16113ts@apps.kct.ac.jp>
2020-08-29 09:27:29 +0900

    2020/08/29

--- interrupt.cpp
+++ interrupt.cpp
@@ -9,16 +9,16 @@ void startTimerInt(){
     MsTimer2::start();
 }
 void timerInt(){
-
+	//ここにタイマー割り込みの処理
 }
 void serialEvent1(){
     if(Serial1.available() > 0){
-        
+        //ここに通信割り込みの処理(Serial1)
     }
 }
 void serialEvent2(){
     if(Serial2.available() > 0){
-        
+        //ここに通信割り込みの処理(Serial2)
     }
 }
 
